67f9f2d0bced5f8789e3434d72471b598e62ffd4
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-07-05 22:38:16 +0200

    Update tricklistRatioStatList.dart

--- lib/stats/tricklist/tricklistRatioStatList.dart
+++ lib/stats/tricklist/tricklistRatioStatList.dart
@@ -1,10 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toscoot/models/results.dart';
-import 'package:toscoot/models/tricklist.dart';
-import 'package:toscoot/services/database.dart';
 import 'package:toscoot/stats/tricklist/tricklistRatioTile.dart';
 
 class TricklistRatioStatList extends StatefulWidget {
